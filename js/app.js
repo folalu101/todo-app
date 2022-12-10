bb8d886d3ce5eed390e43b4a735a3317489bab50
A.Adefolalu B <adefolalucode101@gmail.com>
2022-12-10 15:16:02 +0100

    write function to find list and delete list

--- js/app.js
+++ js/app.js
@@ -8,12 +8,25 @@ function todoList(){
 todoList.prototype.addList = function(list){
     list.id = this.assignId();
     this.lists[list.id] = list;
-}
+};
 
 // todo list prototype to assign id
 todoList.prototype.assignId = function(){
     this.currentId += 1;
     return this.currentId;
-}
+};
 
-// 
+// todo list protptype to find list
+todoList.prototype.findList = function(id){
+    if(this.list[id] != undefined){
+        return this.list[id];
+    }
+    return false;
+};
+
+// todo list prototype to delete list
+todoList.prototype.deleteList = function (id){
+    if(list[id] === undefined){
+        
+    }
+}
