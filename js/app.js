326e38a6e7cfbfd40504e6791b27a62919464ec2
A.Adefolalu B <adefolalucode101@gmail.com>
2022-12-10 15:10:38 +0100

    write a function to assign id

--- js/app.js
+++ js/app.js
@@ -9,3 +9,11 @@ todoList.prototype.addList = function(list){
     list.id = this.assignId();
     this.lists[list.id] = list;
 }
+
+// todo list prototype to assign id
+todoList.prototype.assignId = function(){
+    this.currentId += 1;
+    return this.currentId;
+}
+
+// 
